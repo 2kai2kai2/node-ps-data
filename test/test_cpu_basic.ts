acd399fa19dde79d44ba3435175fae4eab6f48d6
2kai2kai2 <2kai2kai2@gmail.com>
2024-02-20 00:09:04 -0500

    Slow down CPU test
    This should reduce the number of random false failures
    Compensate for slowdown by reducing the number of trials

--- test/test_cpu_basic.ts
+++ test/test_cpu_basic.ts
@@ -24,7 +24,8 @@ const assert = require('assert');
 const pid = proc.pid;
 
 var time = 0;
-const TRIALS = 1000;
+const TRIALS = 200;
+const MAX_FAILURES = 5;
 var failures = 0;
 for (let i = 0; i < TRIALS; i++) {
     let new_time = ps.cpuTime(pid);
@@ -36,14 +37,14 @@ for (let i = 0; i < TRIALS; i++) {
     // And do some extra computation to make sure we get more time.
     let v = 1 + Math.random();
     let u = '{"a": 1}';
-    for (let j = 0; j < 1000; j++) {
-        v = Math.hypot(v, Math.random(), Math.random());
+    for (let j = 0; j < 10000; j++) {
+        v = eval("Math.hypot(v, Math.random(), Math.random())");
         let c = JSON.parse(u);
         c[String.fromCharCode(97 + Math.floor(j * v) % 26)] = v;
         u = JSON.stringify(c);
     }
 }
-if (TRIALS * 0.01 < failures) {
+if (failures > MAX_FAILURES) {
     throw new Error('Too many failures: ' + failures + '/' + TRIALS);
 } else {
     console.log('Basic CPU time test passed with only ' + failures + '/' + TRIALS + ' failures.');
